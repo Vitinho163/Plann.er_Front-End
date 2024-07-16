b86c17bf6e868d2bbf7a0631ee563ce5279d9893
João Victor <victor.jvsb18@gmail.com>
2024-07-16 01:15:53 +0000

    Add responsiveness to Trip Details page

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -23,10 +23,10 @@ export function TripDetailsPage() {
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <DestinationAndDateHeader />
 
-      <main className="flex gap-16 px-4">
+      <main className="flex flex-col lg:flex-row gap-16 px-4">
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
-            <h2 className="text-3xl font-semibold">Atividades</h2>
+            <h2 className="text-xl sm:text-3xl font-semibold">Atividades</h2>
             <Button onClick={openCreateActivityModal} variant="primary">
               <Plus className='size-5' />
               Cadastrar atividade
@@ -36,7 +36,7 @@ export function TripDetailsPage() {
           <Activities />
         </div>
 
-        <div className="w-80 space-y-6">
+        <div className='w-full lg:w-80 space-y-6'>
           <ImportantLinks />
           <div className='w-full h-px bg-zinc-800' />
           <Guests />
@@ -46,9 +46,7 @@ export function TripDetailsPage() {
       <Footer />
 
       {isCreateActivityModalOpen && (
-        <CreateActivityModal 
-          closeCreateActivityModal={closeCreateActivityModal}
-        /> 
+        <CreateActivityModal closeCreateActivityModal={closeCreateActivityModal} />
       )}
     </div>
   )
